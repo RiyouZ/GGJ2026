000301ee21e6691e052c36493c97d0a543a74168
LC <1260762612@qq.com>
2026-02-01 21:53:55 +0800

    2153

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -21,20 +21,16 @@ namespace AK
         static const AkUniqueID PLAY_DOLL_SELECT_HIGHLIGHT_SFX = 3522410409U;
         static const AkUniqueID PLAY_DOLL_SKILL_CAST_SFX = 736602215U;
         static const AkUniqueID PLAY_FOXMASK_FIT_SFX = 4227241441U;
+        static const AkUniqueID PLAY_LEVEL_1 = 833230064U;
         static const AkUniqueID PLAY_LIONMASK_FIT_SFX = 2119739952U;
-        static const AkUniqueID PLAY_MASK_DOLL_A_CLEAR_DREAM_1 = 2929091772U;
-        static const AkUniqueID PLAY_MASK_DOLL_A_CLEAR_DREAM_2 = 2929091775U;
-        static const AkUniqueID PLAY_MASK_DOLL_A_CLEAR_DREAM_3 = 2929091774U;
-        static const AkUniqueID PLAY_MASK_DOLL_A_CLEAR_DREAM_4 = 2929091769U;
-        static const AkUniqueID PLAY_MASK_DOLL_A_CLEAR_DREAM_5 = 2929091768U;
         static const AkUniqueID PLAY_MASK_SPECIAL_SFX = 2708270198U;
+        static const AkUniqueID PLAY_MENU_LEVEL_MUSIC = 637012276U;
         static const AkUniqueID PLAY_MOUSEMASK_FIT_SFX = 3410823607U;
         static const AkUniqueID PLAY_SFX_CAPTUREPOINT_TRIGGER = 1185070519U;
         static const AkUniqueID PLAY_SFX_SCENE_TRANSITION_CLOSE = 3443543745U;
-        static const AkUniqueID PLAY_SFX_SCENE_TRANSITION_OPEN = 388703331U;
         static const AkUniqueID PLAY_TABLE_CHAIR_BUMP_SFX = 322227667U;
         static const AkUniqueID PLAY_VICTORY = 2453267296U;
-        static const AkUniqueID STOP_SFX_SCENE_TRANSITION_CLOSE = 524237271U;
+        static const AkUniqueID STOP_MENU_MUSIC = 3945748993U;
     } // namespace EVENTS
 
     namespace BANKS
